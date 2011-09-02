253eef62e3d1685793faacb4702f7b09efb85762
Nick Partridge <nkpart@gmail.com>
2011-09-02 16:09:06 +1000

    .

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,5 @@
 alias ls='ls -G'
 alias ll='ls -lh'
-alias lla='ls -lah'
 alias ..='cd ..;'
 alias ...='.. ..'
 alias ducks='du -cks * | sort -rn|head -11' # Lists the size of all the folders$
@@ -17,7 +16,8 @@ alias profileme="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' 
 
 alias r='rake'
 
-alias sql="echo Okay idjit, it goes like this:
+alias sql="echo SQL for Nick
+echo ----
 echo CREATE TABLE tablename \(id INTEGER, name VARCHAR\)
 echo SELECT row FROM tablename WHERE condition
 echo INSERT INTO tablename \(column1, column2\) VALUES \(value1, value2\)
@@ -26,33 +26,24 @@ echo UPDATE tablename SET column1 = value1 WHERE condition
 
 alias gl='git pull'
 alias gst='git status -sb'
-alias glr='git pull --rebase'
 alias glg='git lg'
-alias gsu='git submodule update'
-
 alias gp='git push'
-alias gpa='git push-all' # see [alias] in ~/.gitconfig
 alias ga='git add'
 alias gaa='git add -A'
 alias gc='git commit -v'
 alias gb='git branch'
 alias gba='git branch -a'
 alias gco='git checkout'
-
-alias gitx="open -b nl.frim.GitX"
-alias gm='git merge --no-ff'
 alias gfo='git fetch origin'
 alias gdi='git diff --staged'
 alias gd='git diff'
-
-alias gsp='git smart-pull'
+alias gsp='git smart-pull' # From the git-smart gem, $ gem install git-smart
 
 alias days="git log --since='2 month ago' --author=nkpart | grep Date | awk '{print \$2, \$3, \$4}' | uniq"
 alias my_work="git log --since='2 month ago' --date=short --author=nkpart --pretty=format:\"%ad\" | uniq"
 
 alias ox="open *.xcodeproj || open iPhone/*.xcodeproj || open iPadPrototype/*.xcodeproj"
-alias ea='vim ~/p/dot-files/aliases.sh && source ~/p/dot-files/aliases.sh'
-alias deps='cd ~/.babushka/deps && vim'
+alias ea='vim ~/.aliases.sh && source ~/.aliases.sh'
 
 function go () {
   PROJECT_DIRS="$HOME/p/mogen/projects $HOME/p/mogen/projects/oomph-build/mags $HOME/railscamp $HOME/p/mogen/kits $HOME/p"
